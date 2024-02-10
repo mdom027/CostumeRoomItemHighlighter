8c771469ed6a8ff8ebf2a9b10ddeac01d00065b5
mdom <foobar@example.com>
2024-02-09 17:20:51 -0700

    Added sections to config menu.

--- runelite-client/src/main/java/net/runelite/client/plugins/CostumeRoomItemHighlighter/CostumeRoomItemHighlighterConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/CostumeRoomItemHighlighter/CostumeRoomItemHighlighterConfig.java
@@ -3,6 +3,7 @@ package net.runelite.client.plugins.CostumeRoomItemHighlighter;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 import net.runelite.client.plugins.CostumeRoomItemHighlighter.Cases.*;
 
 import java.awt.Color;
@@ -20,8 +21,14 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	{
 		return Color.cyan;
 	}
+	@ConfigSection(
+			name = "Armor Case",
+			description = "Armor Case Settings",
+			position = 2
+	)
+	String armorCase = "armorCase";
 	@ConfigItem(
-			position = 2,
+			position = 3,
 			keyName = "HighlightArmorCaseItems",
 			name = "Highlight Armor Case Items",
 			description = "Highlight all items that can be stored in the Armor Case."
@@ -32,21 +39,29 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 3,
+			position = 4,
 			keyName = "ArmorCaseType",
 			name = "Armor Case Type",
-			description = "Choose the type of Armor Case you have installed."
+			description = "Choose the type of Armor Case you have installed.",
+			section = armorCase
 	)
 	default ArmorCaseType armorCaseType()
 	{
 		return ArmorCaseType.OAK;
 	}
+	@ConfigSection(
+			name = "Cape Rack",
+			description = "Cape Rack Settings",
+			position = 5
+	)
+	String capeRack = "capeRack";
 
 	@ConfigItem(
-			position = 4,
+			position = 6,
 			keyName = "HighlightCapeRackItems",
 			name = "Highlight Cape Rack Items",
-			description = "Highlight all items that can be stored in the Cape Rack."
+			description = "Highlight all items that can be stored in the Cape Rack.",
+			section = capeRack
 	)
 	default boolean highlightCapeRackItems()
 	{
@@ -54,21 +69,29 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 5,
+			position = 7,
 			keyName = "CapeRackType",
 			name = "Cape Rack Type",
-			description = "Choose the type of Cape Rack you have installed."
+			description = "Choose the type of Cape Rack you have installed.",
+			section = capeRack
 	)
 	default CapeRackType capeRackType()
 	{
 		return CapeRackType.OAK;
 	}
+	@ConfigSection(
+			name = "Fancy Dress Box",
+			description = "Fancy Dress Box Settings",
+			position = 8
+	)
+	String fancyDressBox = "fancyDressBox";
 
 	@ConfigItem(
-			position = 6,
+			position = 9,
 			keyName = "HighlightFancyDressBoxItems",
 			name = "Highlight Fancy Dress Box Items",
-			description = "Highlight all items that can be stored in the Fancy Dress Box."
+			description = "Highlight all items that can be stored in the Fancy Dress Box.",
+			section = fancyDressBox
 	)
 	default boolean highlightFancyDressBoxItems()
 	{
@@ -76,21 +99,29 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 7,
+			position = 10,
 			keyName = "FancyDressBoxType",
 			name = "Fancy Dress Box Type",
-			description = "Choose the type of Fancy Dress Box you have installed."
+			description = "Choose the type of Fancy Dress Box you have installed.",
+			section = fancyDressBox
 	)
 	default FancyDressBoxType fancyDressBoxType()
 	{
 		return FancyDressBoxType.OAK;
 	}
+	@ConfigSection(
+			name = "Magic Wardrobe",
+			description = "Magic Wardrobe Settings",
+			position = 11
+	)
+	String magicWardrobe = "magicWardrobe";
 
 	@ConfigItem(
-			position = 8,
+			position = 12,
 			keyName = "HighlightMagicWardrobeItems",
 			name = "Highlight Magic Wardrobe Items",
-			description = "Highlight all items that can be stored in the Magic Wardrobe."
+			description = "Highlight all items that can be stored in the Magic Wardrobe.",
+			section = magicWardrobe
 	)
 	default boolean highlightMagicWardrobeItems()
 	{
@@ -98,21 +129,29 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 9,
+			position = 13,
 			keyName = "MagicWardrobeType",
 			name = "Magic Wardrobe Type",
-			description = "Choose the type of Magic Wardrobe you have installed."
+			description = "Choose the type of Magic Wardrobe you have installed.",
+			section = magicWardrobe
 	)
 	default MagicWardrobeType magicWardrobeType()
 	{
 		return MagicWardrobeType.OAK;
 	}
+	@ConfigSection(
+			name = "Toy Box",
+			description = "Toy Box Settings",
+			position = 14
+	)
+	String toyBox = "toyBox";
 
 	@ConfigItem(
-			position = 10,
+			position = 15,
 			keyName = "HighlightToyBoxItems",
 			name = "Highlight Toy Box Items",
-			description = "Highlight all items that can be stored in the Toy Box."
+			description = "Highlight all items that can be stored in the Toy Box.",
+			section = toyBox
 	)
 	default boolean highlightToyBoxItems()
 	{
@@ -120,21 +159,29 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 11,
+			position = 16,
 			keyName = "ToyBoxType",
 			name = "Toy Box Type",
-			description = "Choose the type of Toy Box you have installed."
+			description = "Choose the type of Toy Box you have installed.",
+			section = toyBox
 	)
 	default ToyBoxType ToyBoxType()
 	{
 		return ToyBoxType.OAK;
 	}
+	@ConfigSection(
+			name = "Treasure Chest",
+			description = "Treasure Chest Settings",
+			position = 17
+	)
+	String treasureChest = "treasureChest";
 
 	@ConfigItem(
-			position = 12,
+			position = 18,
 			keyName = "HighlightTreasureChestItems",
 			name = "Highlight Treasure Chest Items",
-			description = "Highlight all items that can be stored in the Treasure Chest."
+			description = "Highlight all items that can be stored in the Treasure Chest.",
+			section = treasureChest
 	)
 	default boolean highlightTreasureChestItems()
 	{
@@ -142,10 +189,11 @@ public interface CostumeRoomItemHighlighterConfig extends Config
 	}
 
 	@ConfigItem(
-			position = 13,
+			position = 19,
 			keyName = "TreasureChestType",
 			name = "Treasure Chest Type",
-			description = "Choose the type of Treasure Chest you have installed."
+			description = "Choose the type of Treasure Chest you have installed.",
+			section = treasureChest
 	)
 	default TreasureChestType treasureChestType()
 	{
